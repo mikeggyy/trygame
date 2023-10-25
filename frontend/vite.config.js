56ef06abdfcd034ebc6f6266aa652ae508a3a99a
mike123 <>
2023-10-25 15:38:33 +0800

    修改輸出

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   publicPath: process.env.VITE_APP_BASE_URL,
+  base: './', // 设置根路径
   plugins: [
     vue(),
   ],
