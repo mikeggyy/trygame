d609ceb9e2e1a073e2c08465f904dc7a3bdfd127
mike123 <>
2023-10-25 17:28:06 +0800

    幕府重臣不隨機 各藩資料待捕

--- frontend/src/stores/portStore.js
+++ frontend/src/stores/portStore.js
@@ -3,7 +3,27 @@ export const port = defineStore({
   id: 'port',
   state: () => ({
     portList: [
-      { name: '界港', owner: '幕府直轄' }
+      { name: '界港', owner: '勘定奉行', status: '可交易' },
+      { name: '仙台藩', owner: '仙台藩', status: '不可交易' },
+      { name: '府中藩', owner: '府中藩', status: '不可交易' },
+      { name: '鳥取藩', owner: '鳥取藩', status: '不可交易' },
+      { name: '福岡藩', owner: '福岡藩', status: '不可交易' },
+      { name: '長崎藩', owner: '長崎藩', status: '不可交易' },
+      { name: '薩摩藩', owner: '薩摩藩', status: '不可交易' },
+      { name: '岡山藩', owner: '岡山藩', status: '不可交易' },
+      { name: '紀伊藩', owner: '勘定奉行', status: '不可交易' },
+      { name: '石見藩', owner: '石見藩', status: '不可交易' },
+      { name: '宇和島藩', owner: '宇和島藩', status: '不可交易' },
+      { name: '長州藩', owner: '長州藩', status: '不可交易' },
+      { name: '大名藩', owner: '大名藩', status: '不可交易' },
+      { name: '江戶藩', owner: '勘定奉行', status: '不可交易' },
+      { name: '近江藩', owner: '近江藩', status: '不可交易' },
+      { name: '甲府藩', owner: '甲府藩', status: '不可交易' },
+      { name: '小倉藩', owner: '小倉藩', status: '不可交易' },
+      { name: '佐賀藩', owner: '佐賀藩', status: '不可交易' },
+      { name: '熊本藩', owner: '熊本藩', status: '不可交易' },
+      { name: '萩藩', owner: '萩藩', status: '不可交易' },
+      { name: '福山藩', owner: '福山藩', status: '不可交易' },
     ]
   }),
   actions: {
--- server/generateShogun.js
+++ server/generateShogun.js
@@ -2,90 +2,478 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const surnames = [
-  "菅谷", "小幡", "多田", "松浦", "米谷", "別所", "吉岡", "宮崎", "正木", "吉弘",
-  "沼田", "池田", "成富", "佐瀨", "新納", "籾井", "內藤", "太田", "戶澤", "丸目",
-  "結城", "伊藤", "小梁川", "德山", "氏家", "大谷", "京極", "長尾", "大久保",
-  "朝倉", "畠山", "和田", "山內", "大內", "大村", "寺沢", "石倉", "岩崎",
-  "石橋", "石川", "石田", "石原", "岩田", "石井", "石上", "岩下", "柳權",
-  "荻原", "堀田", "酒井", "山火內", "豬葉", "亞田", "丹羽", "中岡", "田淵",
-  "吉良", "諏訪", "大田原", "中村", "圓城寺", "佐藤", "筑紫", "春日", "蒲池"
-];
-const warriorNames = [
-  "基信", "賢豐", "忠成", "治長", "顯綱", "舜範", "長忠", "廣綱", "重隆", "康英",
-  "勝貞", "隆矩", "隆樹", "隆太", "隆志", "隆司", "氏真", "光圀", "家光", "中治",
-  "家綱", "綱吉", "家宣", "家継", "家重", "家治", "家斉", "家慶", "家定", "家茂",
-  "慶喜", "家榮", "義達", "家明", "家久", "家隆", "家恆", "田定", "吉保", "重秀",
-  "景春", "政清", "孝家", "正柱", "定壽", "忠景", "雪応", "義友", "久長", "義央",
-  "資孝", "詮真", "為信", "良直", "信綱", "政景", "鎮漣", "惟光", "高增", "為春",
-  "實充", "具藤", "貞能", "良候", "鑑種", "清良", "房實", "友通", "壽昌", "政賴",
-  "元嘉", "長泰", "藤兼", "資保", "茂季", "元鄰", "義銀", "胤清", "嘉明", "康長",
 
-];
 const officialPosition = [
   "老中", "側用人", "若年寄", "大目付", "留守居役", "勘定吟味役", "勘定奉行",
   "作事奉行", "寺社奉行", "北町奉行", "南町奉行", "火附盜賊改", "世話役"
 ]
-const getRandomName = () => {
-  const randomSurnameIndex = Math.floor(Math.random() * surnames.length);
-  const randomWarriorNameIndex = Math.floor(Math.random() * warriorNames.length);
-  const surname = surnames[randomSurnameIndex];
-  const warriorName = warriorNames[randomWarriorNameIndex];
-  return `${surname}${warriorName}`;
-};
 
-const generateShogun = (count) => {
-  const peopleList = [];
-  // 確定符合條件的職位
-  const eligiblePositions = officialPosition.slice(0, count);
-  for (let i = 0; i < count; i++) {
-    let isNameUnique = false;
-    let randomName = '';
-    let position = '';
-
-    // 检查名字是否唯一，如果不是，继续生成新名字
-    while (!isNameUnique) {
-      randomName = getRandomName();
-      isNameUnique = !peopleList.some(body => body.name === randomName);
-    }
-
-
-    // 檢查是否還有符合條件的職位
-    if (i < eligiblePositions.length) {
-      position = eligiblePositions[i];
-    }
-
-    // 生成年齡
-    const randomAge = Math.floor(Math.random() * 40) + 20;
-    const randomAbility = Math.floor(Math.random() * 81) + 20;
-    const randomPay = Math.floor(Math.random() * 15) + 30;
-    const randomHealth = Math.floor(Math.random() * 40) + 60;
-    const body = {
-      id: i + 1,
-      name: `${randomName}`,
-      age: randomAge,
-      ability: randomAbility,
-      pay: randomPay,
+const generateShogun = () => {
+  let body = []
+  body = [
+    {
+      id: 1,
+      name: '土屋政直',
+      age: 48,
+      ability: 100,
+      pay: 500,
       // 薪水限制
       paylimit: 500,
       // 人物好感度
-      howMuchLike: 200,
+      howMuchLike: 0,
       // 人物健康度
-      health: randomHealth,
+      health: 80,
       smuggling: false,
       limit: '',
       headImg: 'shogun-001.png',
       type: '幕府重臣',
-      profession: position,
+      profession: '老中',
       description: '幕府重臣',
       status: '',
       doYouKnow: false
-    };
-
-    peopleList.push(body);
-  }
-
-  return peopleList;
+    },
+    {
+      id: 2,
+      name: '柳澤吉保',
+      age: 52,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '側用人',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 3,
+      name: '酒井忠真',
+      age: 39,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '若年寄',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 4,
+      name: '水野守政',
+      age: 36,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '大目付',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 5,
+      name: '加賀爪直澄',
+      age: 43,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '留守居役',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 6,
+      name: '關孝和',
+      age: 34,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '勘定吟味役',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 7,
+      name: '稻生正照',
+      age: 37,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '勘定奉行',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 8,
+      name: '木全忠澄',
+      age: 44,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '作事奉行',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 9,
+      name: '水野忠春',
+      age: 45,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '寺社奉行',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 10,
+      name: '遠山景元',
+      age: 32,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '北町奉行',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 11,
+      name: '小栗忠順',
+      age: 49,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '南町奉行',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 12,
+      name: '土岐賴稔',
+      age: 32,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '火附盜賊改',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 13,
+      name: '井上源三郎',
+      age: 43,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '世話役',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 14,
+      name: '小出英持',
+      age: 21,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 15,
+      name: '毛利政苗',
+      age: 25,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 16,
+      name: '松平乘佑',
+      age: 20,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 17,
+      name: '牧野惟成',
+      age: 22,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 18,
+      name: '立花種周',
+      age: 23,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 19,
+      name: '大久保忠真',
+      age: 18,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 20,
+      name: '青山幸哉',
+      age: 17,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 21,
+      name: '黑田直邦',
+      age: 19,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+    {
+      id: 22,
+      name: '仙石政房',
+      age: 20,
+      ability: 100,
+      pay: 500,
+      // 薪水限制
+      paylimit: 500,
+      // 人物好感度
+      howMuchLike: 0,
+      // 人物健康度
+      health: 80,
+      smuggling: false,
+      limit: '',
+      headImg: 'shogun-001.png',
+      type: '幕府重臣',
+      profession: '',
+      description: '幕府重臣',
+      status: '',
+      doYouKnow: false
+    },
+  ]
+  return body;
 }
-
 module.exports = generateShogun;
